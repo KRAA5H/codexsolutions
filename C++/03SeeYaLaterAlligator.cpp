fbe5b4866c1ecc511870e94ac87ddb37ef1d9302
KRAA5H <56270889+KRAA5H@users.noreply.github.com>
2025-01-19 21:25:20 +0000

    Add C++ sample programs with basic output

---
+++ C++/03SeeYaLaterAlligator.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+ std::cout << "🔥 AC/DC - Highway to Hell\n";
+ std::cout << "🌈 Pink Floyd - Comfortably Numb\n";
+ std::cout << "🚓 The Police - Message in a Bottle\n";
+ std::cout << "🤘 The Clash - London Calling\n";
+ std::cout << "💔 Pat Benatar - Heartbreaker\n"; 
+}
