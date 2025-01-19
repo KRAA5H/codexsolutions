cc3afb48620bec8689df27caa1b81c10e46993a3
KRAA5H <56270889+KRAA5H@users.noreply.github.com>
2025-01-19 21:13:25 +0000

    Add DataTypes.js with initial company data and console logs

---
+++ Javascript/DataTypes.js
@@ -0,0 +1,12 @@
+
+let companyName = 'gameChanger';
+let isActive = true;
+const companyFounded = "01/23/2022";
+const foundingYear = 2022;
+let fundingAmount;
+
+console.log(companyName); // gameChanger
+console.log(isActive); // true
+console.log(companyFounded); // 01/23/2022
+console.log(foundingYear); // 2022
+console.log(fundingAmount); // undefined    
